cdb93ba1ba98a798e273a93a5e58ca7c5e55d6a1
Joshua Hoblitt <jhoblitt@cpan.org>
2013-08-02 11:39:34 -0700

    add trivial pureftpd class rspec

--- spec/classes/pureftpd_spec.rb
+++ spec/classes/pureftpd_spec.rb
@@ -1,1 +1,14 @@
 require 'spec_helper'
+
+describe 'pureftpd' do
+  let(:facts) { {:osfamily=> 'RedHat'} }
+
+  describe 'with no params' do
+    it do
+      should include_class('pureftpd') 
+      should include_class('pureftpd::install') 
+      should include_class('pureftpd::config') 
+      should include_class('pureftpd::service') 
+    end
+  end
+end
