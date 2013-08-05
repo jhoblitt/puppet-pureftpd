c3e8fddcb104f3145941fbfba37058091b40aa68
Joshua Hoblitt <jhoblitt@cpan.org>
2013-08-05 15:29:34 -0700

    improve contexts in pureftpd::config rspec

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -82,20 +82,22 @@ describe 'pureftpd::config' do
 
   # accumutate all of the params and content strings as we test each individual
   # option so we can use them for the next test
-  config_options.each do |option|
-    params = {}
-    params[option.downcase.to_sym] = value
-    content = sprintf("%-19s %s\n", option, value)
+  context 'one option at a time' do
+    config_options.each do |option|
+      params = {}
+      params[option.downcase.to_sym] = value
+      content = sprintf("%-19s %s\n", option, value)
 
-    all_params.merge!(params)
-    all_content += content
+      all_params.merge!(params)
+      all_content += content
 
-    it_behaves_like 'config', params, content
+      it_behaves_like 'config', params, content
+    end
   end
   
   # test all of the known options at once this works because the ordering of
   # options values in the output file is fixed
-  context 'all parameters' do
+  context 'all options' do
     it_behaves_like 'config', all_params, all_content
   end
 
