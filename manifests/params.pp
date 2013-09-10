46443520f5753d87e33dbb7b9b638a54acb08f00
Joshua Hoblitt <jhoblitt@cpan.org>
2013-09-09 17:21:48 -0700

    remove templates/redhat dir since no templates are `$::osfamily` specific

--- manifests/params.pp
+++ manifests/params.pp
@@ -12,16 +12,16 @@ class pureftpd::params {
       $config_dir      = '/etc/pure-ftpd'
       $service_name    = 'pure-ftpd'
 
-      $conf_erb        = 'redhat/pure-ftpd.conf.erb'
+      $conf_erb        = 'pure-ftpd.conf.erb'
       $conf_path       = "${config_dir}/pure-ftpd.conf"
 
-      $ldap_conf_erb   = 'redhat/pure-ftpd.conf.erb'
+      $ldap_conf_erb   = 'pure-ftpd.conf.erb'
       $ldap_conf_path  = "${config_dir}/pureftpd-ldap.conf"
 
-      $mysql_conf_erb  = 'redhat/pure-ftpd.conf.erb'
+      $mysql_conf_erb  = 'pure-ftpd.conf.erb'
       $mysql_conf_path = "${config_dir}/pureftpd-mysql.conf"
 
-      $pgsql_conf_erb  = 'redhat/pure-ftpd.conf.erb'
+      $pgsql_conf_erb  = 'pure-ftpd.conf.erb'
       $pgsql_conf_path = "${config_dir}/pureftpd-pgsql.conf"
     }
     default:{
